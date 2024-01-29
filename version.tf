efde5e70beea79129384d5dacabd70f2f50d5c4b
ElviAadj <microspatula@yahoo.com>
2024-01-29 21:36:41 +0000

    terraform

--- version.tf
+++ version.tf
@@ -1,6 +1,13 @@
- # provider block
-
-provider "aws" {
-  region = "us-east-1"
+terraform {
+    backend "s3" {
+    bucket = "mybucket"
+    key    = "Dev/DevOp-Jenk"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.7.0"
+    }
+  }
 }
-
